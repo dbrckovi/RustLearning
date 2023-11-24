79b04e42d3fde2a12d344f542e58df8c6f290cbc
Brc <dbrckovi@gmail.com>
2023-11-25 00:20:45 +0100

    asd

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+    
+    let buffer: &str = "test";
+    println!("{}", buffer);
+
+    let buffer: &str = "aaa";
+    println!("{}", buffer);
+
+    let buffer: &str = "ccc";
+    println!("{}", buffer);
+}
